70fd165584a89f2ad4618b92a7b6ff822a4b146c
Nick Alexandrescu <laur.alexandrescu@gmail.com>
2017-06-28 14:25:26 +0100

    Finished version 1 ready for testing

--- ErrLogParser/ErrLogParser.ps1
+++ ErrLogParser/ErrLogParser.ps1
@@ -1,12 +1,7 @@
 # Nick (Laurentiu) Alexandrescu
 # Trim the content of log files files in the current folder 
-<<<<<<< HEAD
 # and copy them in an output folder
 # In _ErrParseOut\errors.txt you can find the summary of the errors
-=======
-# and copy them in a new folder called trimmed
-# In trimmed\errors.txt you can find the summary of the errors
->>>>>>> 50219519c7a2580511540519d13882626761df8d
 
 cls
 
@@ -16,11 +11,11 @@ $logExt = (Read-Host "`nWhat is the log file extension?[lst]").tolower().Trim(".
 if ((!$logExt) -or ($logExt.Length -gt 4)) { $logExt = "lst" } 
 
 #Exit if no files with mentioned extension are found in the current folder
-<<<<<<< HEAD
 if (!(Test-Path ".\*.$logExt")) { Read-Host "`nNo *.$logExt files in the current folder `n`nPress Enter..."; Exit}
 
 #Intializing variables
-$errArr = @()
+$errRegex = @()
+$outputDir = ".\_ErrParseOut"
 
 #Read error strings from the errors.csv file
 try {
@@ -29,9 +24,9 @@ try {
         #If emtpy line or commented line, skip it
 	    if (($_ -match "^\s*$") -or ($_ -match "^(.s)*#.*$")) { return }
     
-        $errArr += $_.split(",").trim(); 
+        $errRegex += $_.split(",").trim() 
 
-        Write-Host $errArr
+        Write-Host $errRegex
 
     }
 } catch {
@@ -39,6 +34,8 @@ try {
         Read-Host "`nNo Error file Present`n`nPress Enter..."
         Break
 }
+
+$errRegex = $errRegex -join "|"
 
 #Split file template
 if (Test-Path .\splitTemplate.txt) { 
@@ -52,81 +49,52 @@ if (Test-Path .\splitTemplate.txt) {
     }
 }
 
-   
 #Delete old outup folder if it exists
-if (Test-Path .\_ErrParseOut) { Remove-Item -path .\_ErrParseOut -recurse }
+if (Test-Path $outputDir) { Remove-Item -path $outputDir -recurse }
 
 # Creates trimmed folder and errors files
-New-Item -path . -name _ErrParseOut -type directory
-New-Item -path .\_ErrParseOut -name errors_trimmed.txt -type file
-New-Item -path .\_ErrParseOut -name errors_full.txt -type file
-=======
-if (!(Test-Path ".\*.$logExt")) { Read-Host "`nNo *.$logExt files in the current folder `n`nPress Any Key..."; Exit}
-
-#Read error strings from the errors.csv file
-try {
-(gc .\error.csv) | Foreach-Object { }
-} catch {"Error Message: "+$_.Exception.Message+" Failed Item: "+$_.Exception.ItemName|Write-Host; Break} 
-
-#Delete old folder if it exists
-if (Test-Path .\_Parsed) { Remove-Item -path .\_Parsed -recurse }
-
-# Creates trimmed folder and errors files
-New-Item -path . -name _Parsed -type directory
-New-Item -path .\_Parsed -name errors_trimmed.txt -type file
-New-Item -path .\_Parsed -name errors_full.txt -type file
-
-# Ask if you want to trim the file names or not
-do {
-    $trimLogName = (Read-Host "`nTrim log file names? Y/[N] >").tolower()
-} while ($trimLogName -ne "y" -and $trimLogName -ne "n" -and $trimLogName -ne "")
->>>>>>> 50219519c7a2580511540519d13882626761df8d
+New-Item -path . -name $outputDir.Substring(2) -type directory
+New-Item -path $outputDir -name errAll.txt -type file
+New-Item -path $outputDir -name errSplitFile.txt -type file
 
 # Gets all the log files and for each of them ...
-$files = Get-ChildItem .\*.LST
+$files = Get-ChildItem ".\*.$logExt"
 foreach ($file in $files){
-	# Creates a new log file in trimmed folder
-	New-Item -path .\trimmed -name $file.name -type file
 	
-	# contentFlag used for monitor the content of the log file
-	$contentFlag = 0;
-	
+    # Reset splitfile content flag
+    $contentFlag = $FALSE;
+
+	# If beginSplit variable is present this means a trimmed file will be created
+    if($beginSplit) { New-Item -path $outputDir -name $file.name -type file;}
+            	
 	# Gets all the lines of the log file and for each of them ... 
-	$lines = Get-Content $file.FullName
-	foreach ($line in $lines) {
-		$lineLowerCase = $line.ToLower()
+	Get-Content $file.FullName -ReadCount 100| Foreach-Object {
+        
+        if  ($_ -match "$beginSplit|$endSplit|$errRegex") {
+                
+            foreach ($line in $_) {
 		
-		# Check for the actual beginning of the log and set the content contentFlag
-		If($lineLowerCase.Contains("start of deployment scripts")) { $contentFlag = 1 }
+		        # Check for beginning of splitlog file and set the contentFlag
+		        if($beginSplit -and ($line -match $beginSplit)) { $contentFlag = $TRUE }
 
-		# Copy only the log content in the new file
-		If($contentFlag -eq 1) {
-			if ($file.name.length -gt 12 -and $trimLogName -eq "y" ) {
-				$fileName = ($file.name.Substring(0,12)+".LST")
-			}
-			else {
-				$fileName = $file.name
-			}
-			Add-Content -path (".\trimmed\" + $fileName) -value $line
-		}
+		        # Copy only the log content in the new file
+		        if($contentFlag) { Add-Content -path ("$outputDir\" + $file.name) -value $line }
 		
-		# Check for errors and insert into errors files
-		If(($lineLowerCase.Contains("ora-") -or $lineLowerCase.Contains("pls-") -or $lineLowerCase.Contains("sp2-")`
-		-or $lineLowerCase.Contains("unable") -or $lineLowerCase.Contains("warning") -or $lineLowerCase.Contains("dropped")`
-		-or $lineLowerCase.Contains("0 rows updated") -or $lineLowerCase.Contains("dbs-"))) {
-
-			Add-Content -path .\trimmed\errors_full.txt -value ($file.name+": "+$line)
-			If($contentFlag -eq 1) {
-				Add-Content -path .\trimmed\errors_trimmed.txt -value ($file.name+": "+$line)
-			}
-		}
-		
-		# Check for the actual beginning of the log and set the content contentFlag
-		If($lineLowerCase.Contains("end of deployment scripts")) { $contentFlag = 0 }
+		        # Check for errors and insert into errors files
+                if($line -match $errRegex) {
+                    Add-Content -path "$outputDir\errAll.txt" -value ($file.name+": $line")
+		            if($contentFlag) { Add-Content -path "$outputDir\errSplitFile.txt" -value ($file.name+": $line") }
+                }
+        
+		        # Check for ending of splitlog file and unset the contentFlag
+		        if($endSplit -and ($line -match $endSplit)) { $contentFlag = $FALSE }
+            }
+        }
+        elseif($contentFlag) { Add-Content -path ("$outputDir\" + $file.name) -value $_ } 
 	}
 	
 	# Delete empty files
-	If((Get-Content -path (".\trimmed\" + $file.name)) -eq $Null){
-		Remove-Item -path (".\trimmed\" + $file.name)
+	if((Get-Content -path ("$outputDir\" + $file.name)) -eq $Null){
+		Remove-Item -path ("$outputDir\" + $file.name)
 	}
 }
