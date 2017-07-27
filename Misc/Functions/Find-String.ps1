0e5b7367e2c8b1081ce7e7396aefd366c53e0a0b
Nick Alexandrescu <laur.alexandrescu@gmail.com>
2017-07-27 15:59:13 +0100

    Change synopsis and add parameter validation

--- Misc/Functions/Find-String.ps1
+++ Misc/Functions/Find-String.ps1
@@ -1,32 +1,40 @@
-<#
-.Synopsis
-	Find all the files that contain a given string
-.DESCRIPTION
-	Find all the files that contain a given string
-	The search is done reccursive
-	-Files <file_name> can use * as wildcard
-.EXAMPLE
-	Find-String -SearchString break
-	Will search string "break" on all files including subdirectories 
-.EXAMPLE
-	Find-String -SearchString "break|write"
-	Will search string "break" or "write" or on all files including subdirectories
-.EXAMPLE
-	Find-String -Files *.ps1 -SearchString child
-	Will search string "child" on all files with ps1 as extension including subdirectories
-#>
+##############################
+#.SYNOPSIS
+#Find all the files that contain a given string
+#
+#.DESCRIPTION
+#Find all the files that contain a given string
+#The search is done reccursive
+#-Files <file_name> can use * as wildcard
+#
+#.PARAMETER Files
+#Extension of files searched, default is *.*, multiple extensions allowed *.lst
+#
+#.PARAMETER SearchString
+#String searched, default is null and will prompt the usage
+#
+#.EXAMPLE
+#Find-String -SearchString break
+#Will search string "break" on all files including subdirectories 
+#
+#.EXAMPLE
+#Find-String -SearchString "break|write"
+#Will search string "break" or "write" or on all files including subdirectories
+#
+#.EXAMPLE
+#Find-String -Files *.ps1 -SearchString child
+#Will search string "child" on all files with ps1 as extension including subdirectories
+#
+#.NOTES
+#General notes
+##############################
 function Find-String {
 	Param (
-		# Extension of files searched, default is *.*, multiple extensions allowed *.lst
-        $Files = "*.*",
-        # String searched, default is null and will promt the usage
-        $SearchString = $Null
+		[ValidatePattern("\w+")] [String] $Files = "*.*",
+        [String] $SearchString = $Null
 	)
-
-	# Search String cannot be null
-	if (!$SearchString) { Write-Host "Usage: Find-String [-Files] <String> -SearchString <String>"; break }
 	
 	# Get all the files and search the string in them 
-	Get-ChildItem -recurse -Path $Files | Select-String -pattern $SearchString
+	Get-ChildItem -Recurse -Path $Files | Select-String -pattern $SearchString
 	Read-Host "Press Enter..."
 }
